82527555b35ccb574c175dd92fbfe164315bb221
Sam Fahnestock <samiam21@gmail.com>
2026-05-14 08:49:18 -0400

    Add bypass effect

---
+++ Bypass.cpp
@@ -0,0 +1,65 @@
+#include "Bypass.h"
+
+void Bypass::Setup(daisy::DaisySeed *hardware, DaisyDisplay *daisyDisplay, int *newBpm)
+{
+    hw = hardware;
+    display = daisyDisplay;
+
+    volKnob.Init(hw, volKnobChannel, volLevel, volLevelMin, volLevelMax);
+}
+
+float Bypass::Process(float in)
+{
+    return in * volLevel;
+}
+
+void Bypass::Cleanup()
+{
+}
+
+void Bypass::ConfigureKnobPositions(int volChannel)
+{
+    volKnobChannel = volChannel;
+}
+
+void Bypass::Loop(bool allowEffectControl)
+{
+    // Only adjust if we are in edit mode
+    if (allowEffectControl)
+    {
+        // Knob 1 controls the boost level
+        if (volKnob.SetNewValue(volLevel))
+        {
+            debugPrintlnF(hw, "Updated the boost level to: %f", volLevel);
+            updateEditModeKnobValue(display, 0, volLevel);
+
+            // Update the effect settings
+            effectSettings.knobSettings[0] = volLevel;
+        }
+    }
+}
+
+char *Bypass::GetEffectName()
+{
+    return (char *)"BYPASS";
+}
+
+char **Bypass::GetKnobNames()
+{
+    return (char **)knobNames;
+}
+
+EffectSettings Bypass::GetEffectSettings()
+{
+    // Add volume level to the effect settings
+    effectSettings.knobSettings[0] = volLevel;
+
+    // Return the settings
+    return effectSettings;
+}
+
+void Bypass::SetEffectSettings(EffectSettings effectSettings)
+{
+    // Update volume level from the effect settings
+    volLevel = effectSettings.knobSettings[0];
+}
---
+++ Bypass.h
@@ -0,0 +1,53 @@
+#ifndef BYPASS_H
+#define BYPASS_H
+
+#include "IEffect.h"
+
+/**********************************************
+ * Bypass Effect
+ *
+ * SPST 1 -
+ * SPST 2 -
+ * SPST 3 -
+ * SPST 4 -
+ *
+ * SPDT 1 -
+ * SPDT 2 -
+ *
+ * Knob 1 - Volume
+ * Knob 2 -
+ * Knob 3 -
+ * Knob 4 -
+ *
+ * LED 1 -
+ * LED 2 -
+ * LED 3 -
+ * LED 4 -
+ **********************************************/
+
+class Bypass : public IEffect
+{
+public:
+    void Setup(daisy::DaisySeed *hardware, DaisyDisplay *daisyDisplay = nullptr, int *newBpm = nullptr);
+    void Cleanup();
+    float Process(float in);
+    void Loop(bool allowEffectControl);
+    char *GetEffectName();
+    char **GetKnobNames();
+    EffectSettings GetEffectSettings();
+    void SetEffectSettings(EffectSettings effectSettings);
+    void ConfigureKnobPositions(int volChannel);
+
+private:
+    const char *knobNames[MAX_KNOBS] = {(char *)"VOL", (char *)"", (char *)"", (char *)""};
+
+    const float volLevelMin = 30.0f;
+    const float volLevelMax = 1.0f;
+    float volLevel = 10.0f;
+
+    int volKnobChannel = KNOB_1_CHN;
+
+    Knob volKnob;
+};
+
+#endif
